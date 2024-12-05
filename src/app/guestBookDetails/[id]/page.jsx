c3026b410c42615012c7a30038892fe92e8643c5
Hayoon <dbs9830@gmail.com>
2024-12-05 19:11:29 +0900

    persist - 새로고침해도로그인안풀리게

--- src/app/guestBookDetails/[id]/page.jsx
+++ src/app/guestBookDetails/[id]/page.jsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/navigation';
 
 function Page({ params }) {
   const LOCAL_API_BASE_URL = process.env.NEXT_PUBLIC_LOCAL_API_BASE_URL;
+  const LOCAL_IMG_URL = process.env.NEXT_PUBLIC_LOCAL_IMG_URL;
   const [item, setItem] = useState(null); // 데이터 상태
   const [loading, setLoading] = useState(true); // 로딩 상태
   const [error, setError] = useState(null); // 에러 상태
@@ -121,20 +122,46 @@ function Page({ params }) {
             </TableRow>
             <TableRow>
               <TableCell className="table-header">내용</TableCell>
-              <TableCell className="table-cell">{item.gb_content}</TableCell>
+              {/* 줄바꿈 기억 :  <pre> 태그  */}
+              <TableCell className="table-cell"><pre>{item.gb_content}</pre></TableCell>   
             </TableRow>
             <TableRow>
               <TableCell className="table-header">이메일</TableCell>
               <TableCell className="table-cell">{item.gb_email}</TableCell>
             </TableRow>
             <TableRow>
-              <TableCell className="table-header">비밀번호</TableCell>
-              <TableCell className="table-cell">{item.gb_pw}</TableCell>
-            </TableRow>
-            <TableRow>
               <TableCell className="table-header">날짜</TableCell>
               <TableCell className="table-cell">{item.gb_regdate.substring(0, 10)}</TableCell>
             </TableRow>
+            {item.gb_filename && (
+              <TableRow>
+                <TableCell className="table-header">Image</TableCell>
+                <TableCell className="table-cell">
+                  {isAuthenticated ? (
+                    <a
+                      href={`${LOCAL_API_BASE_URL}/guestbook/download/${item.gb_filename}`} // Spring Boot 다운로드 링크
+                      download={item.gb_filename} // 다운로드 파일 이름 지정
+                      target="_blank" // 새 창에서 오픈
+                      rel="noopener noreferrer" // 보안 향상을 위해 추가
+                    >
+                      <img
+                        src={`${LOCAL_IMG_URL}/${item.gb_filename}`}
+                        alt="Uploaded Image"
+                        style={{ width: "150px" }}
+                      />
+                    </a>
+                  ) : (
+                    <img
+                      src={`${LOCAL_IMG_URL}/${item.gb_filename}`}
+                      alt="Uploaded Image"
+                      style={{ width: "150px" }}
+                    />
+                  )}
+
+                </TableCell>
+              </TableRow>
+
+            )}
           </TableBody>
         </Table>
       </TableContainer>
--- store/authStore.jsx
+++ store/authStore.jsx
@@ -1,11 +1,40 @@
-import { create } from 'zustand'
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useAuthStore = create((set) => ({
-  user: null, //사용자정보
-  token: null, //JWT 토큰 
-  isAuthenticated: false, //로그인여부
-  login: (user, token) => set({ user, token, isAuthenticated: true }), //로그인 성공시 처리
-  logout: () => set({ user: null, token: null, isAuthenticated: false }), //로그아웃 성공시 처리 
-}))
+
+
+//persist가 새로고침해도 로그아웃이 안 된다.
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,//사용자정보
+      token: null,//JWT 토큰
+      isAuthenticated: false,//로그인여부
+
+      //로그인처리
+      login: (user, token) => {
+        set({
+          user, token, isAuthenticated: true
+        });
+      },
+
+      //로그아웃처리
+      logout:()=>{
+        set({user:null,token:null,isAuthenticated:false});
+
+      //추가로 로컬스토리지에서 삭제 (보안 강화)
+      localStorage.removeItem("auth-storage");
+    },
+
+    //상태를 초기화하는 기능 추가
+    reset:()=>{
+      set({user:null,token:null,isAuthenticated:false});
+    },
+  }),
+  {name:"auth-storage",//로컬스토리지 키 이름 
+    getStorage:()=>localStorage,// 로컬스토리지 사용
+  }
+  )
+);
 
 export default useAuthStore;
