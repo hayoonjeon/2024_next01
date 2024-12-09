1fa3a722052b577644a3dd7b9796fa6833847bcb
Hayoon <dbs9830@gmail.com>
2024-12-09 14:51:25 +0900

    로그인성공 소셜.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,60 +1,60 @@
 "use client"
-//layout.js는 선택이다. (RootLayout 제외)
-//layout이 필요없는 간단한페이지에서 생략가능
-
+// layout.js는 선택이다.(RootLayout 제외)
+// layout 이 필요없는 간단한 페이지에서는 생략 가능
 import Link from "next/link";
 import './globals.css'
-//페이지 전체의 공통 구조를 렌더링할 때 사용
+// 페이지 전체의 공통 구조를 렌더링 할때 사용
 
-//zustand store 호출
+// zustand store 호출
 import useAuthStore from '../../store/authStore';
 import { Button, Stack } from "@mui/material";
-//부모 컴포넌트
+
+// 부모컴포넌트
 export default function RootLayout({ children }) {
-  //zustand 상태가져오기
+  // zustand 상태 가져오기
   const { isAuthenticated, user, logout } = useAuthStore();
 
   const handleLogout = () => {
-    //zustand에 있는 함수 호출
+    // zustand에 있는 함수 호출
     logout();
-    alert('로그아웃되었습니다.')
+    alert("로그아웃 되었습니다.")
   }
-
-
-
   return (
     <html lang="en">
       <body style={{ textAlign: "center" }}>
-        {/* <header style={{marginTop:"50px"}}>공통 헤더</header> */}
-        {/* 자식컴포넌트가 렌더링된다.  */}
-        {/* {children}
-        <footer>공통 푸터</footer> */}
+        {/* <header style={{ marginTop: "50px" }}>공통 헤더</header> */}
+        {/* 자식컴포넌트가 랜더링 된다. */}
+        {/* {children} */}
+        {/* <footer style={{ marginTop: "50px" }}>공통 푸터</footer> */}
+
         <h1><Link href="/">WEB</Link></h1>
         <nav>
-          <Stack direction="row" spacing={2} justifyContent="center" >
-            <Link href={"/read/1"}>HTML</Link>
-            <Link href={"/read/2"}>CSS</Link>
-            <Link href={"/read/3"}>JS</Link>
-            <Link href={"/gallery"}>IMAGE</Link>
-            <Link href={"/itemList"}>ItemList(외부서버)</Link>
-            <Link href={"/guestBookList"}>GuestBook(Spring 서버)</Link>
+          <Stack direction="row" spacing={2} justifyContent="center">
+            <Link href="/read/1"> HTML</Link>
+            <Link href="/read/2">CSS</Link>
+            <Link href="/read/3">JS</Link>
+            <Link href="/gallery"> image </Link>
+            <Link href="/itemList">ItemList(외부서버)</Link>
+            <Link href="/guestBookList">Guestbook(Spring 서버)</Link>
             {isAuthenticated ? (
-              <><span style={{ fontSize: "16px" }}><b>{user.m_id}님 환영합니다</b> </span>
+              <>
+                <span style={{ fontSize: "16px" }}><b>{user.name}님 환영합니다.</b></span>
                 <Button variant="contained" onClick={handleLogout}>logout(Spring 서버)</Button>
               </>
-            ) : (<>
-              <Link href={"/login"}>login(Spring 서버)</Link>
-              <Link href={"/join"}>join(Spring 서버)</Link>
-            </>
+            ) : (
+              <>
+                <Link href="/login">login(Spring 서버)</Link>
+                <Link href="/join">join(Spring 서버)</Link>
+              </>
             )}
-            <Link href={"/kakaoPay"}>카카오페이 연습</Link>
           </Stack>
         </nav>
         <hr />
         {children}
         <hr />
         <ul>
-          {/* /create면 create 폴더를 찾는다.(page.jsx(필수)와 layout.jsx(선택)가 있음) */}
+          {/* /create 이면 create 폴더를 찾는다. 
+            (page.jsx(필수), layout.jsx(선택) 가 있어야한다.*/}
           <li><Link href="/create">Create</Link></li>
           <li>Update</li>
           <li><input type="button" value="delete" /></li>
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,82 +1,93 @@
 "use client"
 import { Avatar, Button, FormControl, Stack, TextField } from '@mui/material';
 import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from 'react';
 
-//zustand store 호출
+// zustand store 호출
 import useAuthStore from '../../../store/authStore';
 
-function page(props) {
-  const LOCAL_API_BASE_URL = process.env.NEXT_PUBLIC_LOCAL_API_BASE_URL
-  const API_URL = `${LOCAL_API_BASE_URL}/members/login`;
-  const router = useRouter(); // useRouter 초기화
+function Page(props) {
+    const LOCAL_API_BASE_URL = process.env.NEXT_PUBLIC_LOCAL_API_BASE_URL
+    const API_URL = `${LOCAL_API_BASE_URL}/members/login`;
+    const router = useRouter(); // useRouter 초기화
+    const { login } = useAuthStore(); // zustand login 함수 가져오기 
 
-  //zustand login함수 가져오기
-  const{login} =useAuthStore();
 
-  
-  //텍스트필드 초기화
-  const initUvo = {
-    m_id: "",
-    m_pw: "",
-  }
-  const [uvo, setUvo] = useState(initUvo);
-  // 모든 입력 필드가 비어있지 않아야 true => 버튼이 활성화
-  const isBtnChk = !uvo.m_id || !uvo.m_pw;
+    // 텍스트필드 초기화
+    const initUvo = {
+        m_id: "",
+        m_pw: ""
+    };
 
-  function changeUvo(e) {
-    const { name, value } = e.target;
-    setUvo(prev => ({
-      ...prev, [name]: value
-    }));
-  }
+    const [uvo, setUvo] = useState(initUvo);
+    const isBtnChk = !uvo.m_id || !uvo.m_pw;
 
-  function goServer(params) {
-    axios.post(API_URL, uvo)
-      .then(response => {
-        const data=response.data;
-        if (data.success) {
-          console.log(data);
-       //   login({id:},data.data.token)
-          alert(data.message);
-          login(data.data,data.token);
-          router.push('/');
-      
-        } else {
-          console.log(data.data);
-          alert(data.data.message);
-          setUvo(initUvo);
+    // URL 쿼리 파라미터에서 토큰 확인 후 처리
+    useEffect(() => {
+        
+        const searchParams = new URLSearchParams(window.location.search);
+        const token = searchParams.get("token");
+        const username = searchParams.get("username");
+        const email = searchParams.get("email");
+        const name = searchParams.get("name");
+
+        if (token && username && email && name) {
+            alert("로그인 성공");
+            // 사용자 정보 생성
+            const user = {
+                username, email, name
+
+            };
+            login(user, token); // Zustand 상태에 저장
+            router.push("/"); // 홈으로 이동
         }
-      });
-  }
+    }, [login, router]);
 
-  function handleKakaoLogin() {
-    //카카오 인증 엔드포인트
-    window.location.href = "http://localhost:8080/oauth2/authorization/kakao";
+    function changeUvo(e) {
+        const { name, value } = e.target;
+        setUvo(prev => ({
+            ...prev, [name]: value
+        }));
+    }
+    function goServer(params) {
+        axios.post(API_URL, uvo)
+            .then(response => {
+                const data = response.data;
+                if (data.success) {
+                    alert(data.message);
+                    login(data.data, data.token);
+                    router.push('/');
+                } else {
+                    alert(data.message);
+                    setUvo(initUvo);
+                }
+            });
+    }
+
+    function handleKakaoLogin() {
+        window.location.href = "http://localhost:8080/oauth2/authorization/kakao";
+    }
+
+    function handleNaverLogin() {
+        window.location.href = "http://localhost:8080/oauth2/authorization/naver";
+    }
+
+    return (
+        <div>
+            <FormControl>
+                {/* 수직정렬 */}
+                <Stack direction="column" spacing={1} alignItems='center'>
+                    <Avatar />
+                    <TextField type='text' label='아이디' name='m_id' value={uvo.m_id} onChange={changeUvo} />
+                    <TextField type='password' label='패스워드' name='m_pw' value={uvo.m_pw} onChange={changeUvo} />
+                    <Button fullWidth variant='contained' disabled={isBtnChk} onClick={goServer}>Sign in</Button>
+                    <Button fullWidth variant='contained' onClick={handleKakaoLogin} style={{ backgroundColor: '#FFEB3B' }}>카카오 로그인</Button>
+                    <Button fullWidth variant='contained' onClick={handleNaverLogin} style={{ backgroundColor: '#03C75A' }}>네이버 로그인</Button>
+                </Stack>
+            </FormControl>
+        </div>
+    );
 }
 
-function handleNaverLogin() {
-    window.location.href = "http://localhost:8080/oauth2/authorization/naver";
-}
-
-  return (
-    <div>
-      <div>
-        <FormControl>
-          {/* 수직정렬 */}
-          <Stack direction="column" spacing={1} alignItems='center'>
-            <Avatar />
-            <TextField type='text' label='아이디' name='m_id' value={uvo.m_id} onChange={changeUvo} />
-            <TextField type='password' label='패스워드' name='m_pw' value={uvo.m_pw} onChange={changeUvo} />
-            <Button fullWidth variant='contained' disabled={isBtnChk} onClick={goServer}>LOGIN</Button>
-            <Button fullWidth variant='contained' onClick={handleKakaoLogin} style={{ backgroundColor: '#FFEB3B', color:'black'}}>카카오 로그인</Button>
-            <Button fullWidth variant='contained' onClick={handleNaverLogin} style={{ backgroundColor: '#03C75A', color:'black' }}>네이버 로그인</Button>
-          </Stack>
-        </FormControl>
-      </div>
-    </div>
-  );
-}
-
-export default page;
+export default Page;
